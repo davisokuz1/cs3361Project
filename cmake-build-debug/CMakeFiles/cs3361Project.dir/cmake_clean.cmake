58ef70c3f731cc7d2bac6cb5c5576960935115da
davisokuz1 <davisokuzor@gmail.com>
2021-11-26 10:28:52 -0600

    add executable file

---
+++ cmake-build-debug/CMakeFiles/cs3361Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs3361Project.dir/main.cpp.obj"
+  "cs3361Project.exe"
+  "cs3361Project.exe.manifest"
+  "cs3361Project.pdb"
+  "libcs3361Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs3361Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
